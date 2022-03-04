e5943a57c9e443fea6cd7d85c095dac1d3a57b7a
Max Safronov <bmc938@gmail.com>
2022-03-04 21:07:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+internal class Program
+{
+
+    private static void Main()
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+        int majElement = 0;
+        int count = 1;
+        int curElement;
+        for (int i = 0; i < N; i++)
+        {
+            int.TryParse(Console.ReadLine(), out curElement);
+            if (curElement == majElement)
+                count++;
+            else
+                count--;
+            if (count == 0)
+            {
+                majElement = curElement;
+                count = 1;
+            }
+            if (i % 1000 == 0)
+                GC.Collect();
+        }
+        Console.WriteLine(majElement);
+    }
+}
